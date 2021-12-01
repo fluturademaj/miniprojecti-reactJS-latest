b13a0a4fd1cf71dadbe9de9a64792593984f7b40
BIT Admin <fluturademaj>
2021-12-01 20:35:57 +0100

    Customers services functions added.

--- src/services/Customers.services.js
+++ src/services/Customers.services.js
@@ -1,1 +1,17 @@
-//Customer services
+import http from "../http-common/http-common"
+export class CostumerDataService {
+
+    getCustomer(){
+        return http.get("/customers");
+    }
+
+    static addCustomer(customer){
+        return http.post("/customers",customer);
+    }
+
+    deleteCustomer(id) {
+        return http.delete("/customers/" + id);
+    }
+}
+
+export default new CostumerDataService();
