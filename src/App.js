96f00c539c1bf4c8d43d5e0a0087be88f45c9124
BIT Admin <fluturademaj>
2021-12-01 19:51:51 +0100

    Add customer component was added.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CustomersComponent from "./components/Customers.component";
 import HeaderComponent from "./components/Header.component";
 import FooterComponent from "./components/Footer.component";
 import ProductsComponent from "./components/Product.component"
+import AddCustomerComponent from "./components/AddCustomer.component";
 
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
                       <Route exact path="/" component={CustomersComponent}/>
                       <Route exact path="/customers" component={CustomersComponent}/>
                       <Route exact path="/products" component={ProductsComponent}/>
+                      <Route exact path="/add-customer" component={AddCustomerComponent}/>
+
 
                   </Switch>
                 </div>
--- src/components/Customers.component.js
+++ src/components/Customers.component.js
@@ -8,9 +8,14 @@ class CustomersComponent extends Component {
         this.state = {
             customers: []
         }
+        this.addCustomer = this.addCustomer.bind(this);
     }
 
     componentDidMount() {
+    }
+
+    addCustomer(){
+        this.props.history.push('/add-customer');
     }
 
 
@@ -32,7 +37,7 @@ class CustomersComponent extends Component {
             <div>
                 <h2 className="text-center">Customers List</h2>
                 <div className="d-flex justify-content-between">
-                    <button className="btn btn-success">
+                    <button className="btn btn-success" onClick={this.addCustomer}>
                         Add Customer
                     </button>
                     <input
